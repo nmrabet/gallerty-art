cc9d5e80832d7de06bf4d98660894a6dcafb8303
Nesrine Mrabet <mrabet_nesrine@yahoo.fr>
2023-07-12 17:24:07 +0100

    Add logo link to homepage

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import logo from "../assets/gallery-logo.png";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <>
       <nav className="navbar">
-        <img src={logo} alt="logo" width={170} height={48} />
+        <Link to="/">
+          <img src={logo} alt="logo" width={170} height={48} />
+        </Link>
         <h5>START SLIDESHOW</h5>
       </nav>
       <hr />
